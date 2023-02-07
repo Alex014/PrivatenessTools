5d7a8e5a46c64d3f78bcc5214fdaba77068c5d3e
Aleksej Sokolov <aleksej000@gmail.com>
2023-02-07 03:50:35 +0200

    Upload and download

---
+++ download.py
@@ -0,0 +1,57 @@
+import os
+import sys
+
+from framework.Container import Container
+from services.ServicesManager import ServicesManager
+
+from NessKeys.exceptions.MyNodesFileDoesNotExist import MyNodesFileDoesNotExist
+from NessKeys.exceptions.NodesFileDoesNotExist import NodesFileDoesNotExist
+from NessKeys.exceptions.NodeNotFound import NodeNotFound
+from NessKeys.exceptions.NodeError import NodeError
+from NessKeys.exceptions.AuthError import AuthError
+
+import requests
+
+class Noder:
+
+    def __manual(self):
+        print("*** File download")
+        print("### USAGE:")
+        print("#### Download file from service node")
+        print(" python download.py <file_id>")
+
+    def process(self):
+
+        if len(sys.argv) == 2 and (sys.argv[1].lower() == 'help' or sys.argv[1].lower() == '-h'):
+            self.__manual()
+
+        elif len(sys.argv) == 2:
+            file_id = sys.argv[1]
+
+            km = Container.KeyManager()
+            sm = ServicesManager(km.getUserLocalKey())
+            
+            try:
+                node_name = km.getCurrentNodeName()
+
+                if sm.joined(km.getNodesKey(), node_name):
+                    sm.download(km.getNodesKey(), km.getMyNodesKey(), node_name, file_id)
+
+            except MyNodesFileDoesNotExist as e:
+                print("MY NODES file not found.")
+                print("RUN python node.py set node-url")
+            except NodesFileDoesNotExist as e:
+                print("NODES LIST file not found.")
+                print("RUN python nodes-update.py node node-url")
+            except NodeNotFound as e:
+                print("NODE '{}' is not in nodes list".format(e.node))
+            except NodeError as e:
+                print("Error on remote node: " + e.error)
+            except AuthError as e:
+                print("Responce verification error")
+
+        else:
+            self.__manual()
+
+upd = Noder()
+upd.process()
